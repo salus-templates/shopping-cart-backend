81bbc6cf163d812ed26cc5d033a98cb0506955e8
Abiola Ibrahim <abiola@salus.cloud>
2025-07-04 11:08:18 +0100

    Fix product model
    Signed-off-by: Abiola Ibrahim <abiola@salus.cloud>

--- ProductService/Product.cs
+++ ProductService/Product.cs
@@ -8,4 +8,5 @@ public class Product
     public decimal Price { get; set; }
     public string ImageUrl { get; set; }
     public string Description { get; set; }
+    public int Stock { get; set; }
 }
